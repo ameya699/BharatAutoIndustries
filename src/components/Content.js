ad9ea7fd92587edac895d6be3bc6fed79fd874af
ameya699 <awatadeameya68@gmail.com>
2024-08-28 18:31:27 +0530

    implemented the carousel and the content on the homepage, changed the font of entire website as per requirement, also made these components responsive

---
+++ src/components/Content.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Content = () => {
+  return (
+    <div className='content-container'>
+        <div className='content-heading'>About the Company...</div>
+        <div><p className='content'>Oil pumps are an important part of every mechanical device that functions on fuel. For effective lubrication of ones machines as well as vehicles, everyone searches for the best oil pumps in the marketplace. Many choose our company, Bharath Auto Industries, as their partner in the market because we promise the supply of a well-engineered assortment of durable oil pumps. The pump models that we bring froth in the market are considered the best. Their flawless finish, efficiency and compact sizes are a few of the prime reasons behind our company's success. The Arjun Oil Pumps, 6C-3948072 Oil Pumps, 4231800501 Oil Pumps, Eicher Oil Pumps, etc., that we have been offering since 1984, are developed for light commercial vehicles, stationary engines, cars, heavy duty trucks and more. </p>
+        <p className='content'>Currently, we are working as an AIATF 16949/ISO: 2015 certified manufacturer and supplier, who is focusing on meeting the specific demands of each and every client at all times. To assure maximum customers delight, we customize the assortment as per each clients individual requirements. The designs, sizes and mechanical specifications of the pumps are subject to customization. Our company is approved by ARAI (Pune) and our client list includes reputed names like Mahindra and Mahindra, Dutz To Dol, Nissan, etc. </p>
+        <p className='content'>As a quality conscious organization, our experienced enterprise maintains the highest standards of quality in every sphere of its activity. We follow well defined parameters of quality control right from the first stage of selecting the raw materials and continue the process till the dispatch of finished products. A team of well trained experts monitors our entire process, thereby ensuring zero defect production at every level. Our organization wants to accelerate its process by satisfying customers and expanding customer base by continuously offering premium grade 3000-D1NN6600D Oil Pumps, 1060 Oil Pumps, etc. </p>
+        </div>   
+    </div>
+  )
+}
+
+export default Content
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import "../App.css"
-
+import ImageSlider from './ImageSlider';
+import Content from './Content';
 
 const Home = () => {
 
   return(
     <div className="fadeInUp-animation ">
-    company homepage
+    <ImageSlider/>
+    <Content/>
     </div>
   )
 
