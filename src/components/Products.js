6b03a931576a15ed6ca6eb7d3114e8eb6aed7c90
ameya699 <awatadeameya68@gmail.com>
2024-09-02 18:46:44 +0530

    added route for the product, implemented the product page, fixed fonts, made mobile responsive

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import Product from './Product'
+import { Link } from 'react-router-dom'
 
 const Products = () => {
+
+  const products=[{image:"imageurl",id:"12345",productname:"Cummins pump"}]
   return (
     <div className='fadeInUp-animation'>
           <div className='productscontainer'>
@@ -17,8 +20,12 @@ const Products = () => {
           <Product/>
           <Product/>
           <Product/>
+          {
+            products.map((ele)=><Link to={`/products/${ele.id}`}><Product key={ele.id}/></Link>)
+          }
           </div>
           </div>
+          
     </div>
   )
 }
